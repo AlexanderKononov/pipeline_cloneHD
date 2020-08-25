cfa8e9c5fdd0adc18819685306e0cb482f85b1bd
Alex <Kononov.nov@mail.ru>
2020-08-25 02:58:58 +0100

    Update joiningSamples.py
    del space in python script

--- pipelineHD/joiningSamples.py
+++ pipelineHD/joiningSamples.py
@@ -1,7 +1,5 @@
 import sys
-
 arg=sys.argv
-
 nfile=len(arg)-1
 coint=0
 file_list=[]
@@ -10,16 +8,11 @@ for i in range(nfile):
 	file_list.append(open(arg[i+1],'r'))
 p_name=arg[1].split('.')[0]
 p_name=p_name+'.'+arg[1].split('.')[1].split('_')[0]+'.txt'
-
 fw=open(p_name,'w')
-
 for f in file_list:
 	line_list.append(f.readline().strip().split('\t'))
-
 flg=0
-
 chr_curr=''
-
 while 0==0:
 	current=[]
 	brk=False
@@ -30,7 +23,6 @@ while 0==0:
 			break
 		if flg==1:
 			print(i)
-	
 		while int(line_list[i][0])<int(line_list[i+1][0]):
 			line_list[i]=file_list[i].readline().strip().split('\t')
 			if len(line_list[i])<2:
@@ -41,7 +33,6 @@ while 0==0:
 			if len(line_list[i+1])<2:
 				brk=True
 				break
-
 		if flg==1:
 			print(str(line_list[i])+'---'+str(line_list[i+1]))
 			print('------')
@@ -54,7 +45,6 @@ while 0==0:
 				break
 		if flg==1:
 			print(str(line_list[i])+'---'+str(line_list[i+1]))
-			
 		while int(line_list[i][1])>int(line_list[i+1][1]):
 			line_list[i+1]=file_list[i+1].readline().strip().split('\t')
 			if len(line_list[i+1])<2:
@@ -65,20 +55,16 @@ while 0==0:
 		if flg==1:
 			print(str(line_list[i])+'---'+str(line_list[i+1]))
 		current=line_list[i+1]
-
 	# Write ontput
 	coint+=1
 	finde_pos=True
 	for i in range(nfile):
 		if line_list[i][0]=='':
 			break
-		
 		if line_list[i][0]!=current[0] or line_list[i][1]!=current[1]:
-			
 			if line_list[0][0]!=chr_curr:
 				chr_curr=line_list[0][0]
 				fw.write(line_list[0][0]+'\t'+line_list[0][1])
-
 				for i in range(nfile):
 					if flg==1:
 						print(line_list[i])
@@ -87,7 +73,6 @@ while 0==0:
 					if len(line_list[i])<2:
 						brk=True
 				fw.write('\n')
-
 			finde_pos=False
 	if flg==1:
 			print(finde_pos)
@@ -102,7 +87,6 @@ while 0==0:
 			if len(line_list[i])<2:
 				brk=True
 		fw.write('\n')
-
 	# exet from cicle
 	brk=False
 	for i in range(nfile):
@@ -111,7 +95,6 @@ while 0==0:
 			break
 	if brk:
 		break
-
 
 for i in file_list:
 	i.close()
