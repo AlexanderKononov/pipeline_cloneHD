087e5b99bf39118aff2fbc3ef0c024d7dd0264ab
Alex <Kononov.nov@mail.ru>
2020-08-25 03:01:59 +0100

    Update filter5.py
    del chank of commented code

--- pipelineHD/code/filter5.py
+++ pipelineHD/code/filter5.py
@@ -21,23 +21,6 @@ while line!= '':
 			break
 		colm=line.strip().split('\t')
 		colm[0]=colm[0].replace('chr','').replace('X', '23').replace('Y', '24').replace('M', '25')
-		#if colm[0]=='24':
-		#	line=f_cvf.readline()
-		#	continue
-		#if colm[6]=='filter4':
-		#	line=f_cvf.readline()
-		#	continue
-		#rg=colm[-1].strip().split(':')
-		#if len(rg)<=3:
-		#	line=f_cvf.readline()
-		#	continue
-		#rb=rg[1].strip().split(',')
-		#if rg[2]=='0':
-		#	line=f_cvf.readline()
-		#	continue
-		#if rb[1]=='0' or rb[1]==rg[2]:
-		#	line=f_cvf.readline()
-		#	continue
 		baf=float(colm[2])/float(colm[3])
 		if baf<0.1 or baf>0.9:
 			line=f_cvf.readline()
@@ -89,38 +72,6 @@ while line!= '':
 	for i in lable_data:
 			if i[2] in del_list_lvl:
 				continue
-			f_baf.write(data[int(i[0])])	
-	'''
-	up_data=np.array([[0,0]])
-	down_data=np.array([[0,0]])
-	for i in lable_data:
-		if i[1]>=0.5:
-			up_data=np.append(up_data, [[i[0],i[1]]], axis=0)
-	mean_baf=lable_data[:,1].mean()
-	var_baf=np.var(lable_data[:,1])
-	mean_up=up_data[:,1].mean()
-	var_up=np.var(up_data[:,1])
-	min_mead_band=mean_baf-2*var_baf
-	max_mead_band=mean_baf+2*var_baf
-	min_up_band=mean_up-2*var_up
-	max_up_band=mean_up+2*var_up
-	coint_up=0
-	coint_mead=0
-	for i in lable_data:
-		if i[1]>min_mead_band and i[1]<max_mead_band:
-			coint_mead+=1
-		if i[1]>min_up_band and i[1]<max_up_band:
-			coint_up+=1
-	#print(len(data))
-	#print(len(lable_data))
-	if coint_mead>=coint_up:
-		for i in lable_data:
-			if i[1]>min_mead_band and i[1]<max_mead_band:
-				f_baf.write(data[int(i[0])])
-	else:
-		for i in lable_data:
-			if (i[1]>min_up_band and i[1]<max_up_band) or (i[1]<(1-min_up_band) and i[1]>(1-max_up_band)):
-				f_baf.write(data[int(i[0])])
-	'''
+			f_baf.write(data[int(i[0])])
 f_cvf.close()
 f_baf.close()
